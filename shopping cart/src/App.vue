f0411512e682fe822df8dc6bcaefd130d167e2ec
Dina El Badri <dinaelbadri94@gmail.com>
2022-12-06 16:35:42 +0100

    shopping cart in progress

---
+++ shopping cart/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import PRODUCTS from "./products.json";
+
+export default {
+  data() {
+    return {
+      products: PRODUCTS,
+      
+    };
+  },
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
